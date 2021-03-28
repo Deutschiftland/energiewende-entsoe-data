325c5824d5725b466e618b448623961e23990110
servanso <63397711+servanso@users.noreply.github.com>
2021-03-28 18:30:55 +0200

    clean up code and add gif

--- data_analyis_request_ENTSO_E.py
+++ data_analyis_request_ENTSO_E.py
@@ -1,19 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from entsoe import EntsoePandasClient
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+from entsoe import EntsoePandasClient
 
-# functions
 def multiply_by_sourceEm(arr,sourceEm,unitConv):
+    """
+    converts power to co2 emission
+
+    Parameters
+    ----------
+    arr : array
+        electricity power per time step for 1 country and 1 source (MW)
+    sourceEm : float
+        co2 intensity for 1 source in Kgco2eq/MWh
+    unitConv : float
+        conversion rate from MW to MWh (x time step) and from kgco2 to tco2 (/1000)
+        warning: time step not the same for all countries, 
+        we use timeStep parameter to define number of time steps per hour (manual entry)
+
+    Returns
+    -------
+    array
+        co2 emission per time step
+
+    """
     return np.multiply(arr,sourceEm*unitConv)
 
 def query_entsoe(start, end, country_list):
+    """
+    queries ENTSOE database if data not already available
+
+    Parameters
+    ----------
+    start : timestamp
+        start of the period queried (day)
+    end : timestamp
+        end of the period queried (day)
+    country_list : list
+        list of countries to retrieve
+
+    Returns
+    -------
+    df : TYPE
+        DESCRIPTION.
+    timespan : TYPE
+        DESCRIPTION.
+
+    """
     df = []
-    for index, country in enumerate(country_list):
+    for country in country_list:
         # check if file already exists
         files = os.listdir('./data')
         timespan = f"{start.strftime('%Y%m%d')}-{end.strftime('%Y%m%d')}"
@@ -26,7 +65,8 @@ def query_entsoe(start, end, country_list):
             dfc = client.query_generation(country, start=start, end=end)
             if dfc.columns.nlevels == 2:
                 # Suppression des valeurs de consommation (de toute manière vides)
-                # Suppression du second niveau pour les noms de colonnes (la mention "Actual Aggregated" qui ne sert à rien)
+                # Suppression du second niveau pour les noms de colonnes 
+                # (la mention "Actual Aggregated" qui ne sert à rien)
                 dfc = dfc.filter(regex='Actual Aggregated', axis=1).droplevel(1,axis=1)
             # write csv file to /data
             dfc.rename(columns= {df.columns[0]:'date'}, inplace=True)
@@ -38,8 +78,8 @@ def query_entsoe(start, end, country_list):
 # CO2 emission of each technology in a dictionary
 # unite: gco2eq/kWh = Kg co2eq/MWh
 # source: https://www.ipcc.ch/site/assets/uploads/2018/02/ipcc_wg3_ar5_annex-iii.pdf
-
-# by default we refer to emission numbers in the table A.III.2 (median of life cycle analyses), otherwise we indicate the source with the numbers
+# by default we refer to emission numbers in the table A.III.2 (median of life cycle analyses), 
+# otherwise we indicate the source with the numbers
 # used the biomass "dedicated", must check what is the dominant tech in each country...
 # used generic "coal" emission for both brown and hard coal,
 # all hydros use same number (24)
@@ -67,55 +107,51 @@ co2PerSource = {
     'Wind Onshore': 11.
     }
 
-# client parameters
+# user parameters
 client = EntsoePandasClient(api_key='${{ secrets.TOKEN_API }}')
+year_start = 2015
+year_end = 2021
+country_code = ['DE','FR']
+timeStep = [1./4.,1.] # quarter hour data for germany, hourly for france
+unitConversion = [t/1000. for t in timeStep]
 
-for year in range(2015,2021):
+for year in range(year_start, year_end):
+    start = pd.Timestamp(year=year, month=1, day=1, tz='Europe/Brussels')
+    end = pd.Timestamp(year=year, month=12, day=31, tz='Europe/Brussels')
+    # if we want monthly, add:
     #for month in range(1,13):
-        # Paramétrage de la période de l'historique demandé
-        # if yearly
-        start = pd.Timestamp(year=year, month=1, day=1, tz='Europe/Brussels')
-        end = pd.Timestamp(year=year, month=12, day=31, tz='Europe/Brussels')
-        # if monthly
         #start = pd.Timestamp(year=year, month=month, day=1, tz='Europe/Brussels')
         #end = pd.Timestamp(year=year, month=month, day=start.daysinmonth, tz='Europe/Brussels')
-        country_code = ['DE','FR']
+    
+    # call query
+    df, timespan = query_entsoe(start, end, country_code)
+    emissionDf = [dfc.copy() for dfc in df]    
 
-        # call query
-        df, timespan = query_entsoe(start, end, country_code)
-
-        emissionDf = [dfc.copy() for dfc in df]
-        # note from MichelB: original table reports power (in MW), we multiply by time step to get energy (in MWh), and then convert to emission in tCo2 = KgCo2/1000
-        # quarter hour data
-        # warning: time step not the same for all countries, use a list of time units in hours
-        timeStep = [1./4.,1.]
-        # we multiply by time unit but divide by 1000 to get tons of CO2
-        unitConversion = [t/1000. for t in timeStep]
-
-        for kcount,countr in enumerate(country_code):
-          for source in df[kcount].keys():
+    # convert from MW to co2 emission per country and source
+    for kcount,countr in enumerate(country_code):
+        for source in df[kcount].keys():
             emissionDf[kcount][source] = df[kcount][[source]].apply(multiply_by_sourceEm, args = (co2PerSource[source],unitConversion[kcount]))
 
-        # write csv file to /data with emissions per source in tons for each time step
-        timespan = f"{start.strftime('%Y%m%d')}-{end.strftime('%Y%m%d')}"
-        [emissionDfc.to_csv(f"./data/{timespan}_{country_code[index]}_emission.csv") for index, emissionDfc in enumerate(emissionDf)]
+    # write csv file to /data with emissions per source in tons for each time step
+    timespan = f"{start.strftime('%Y%m%d')}-{end.strftime('%Y%m%d')}"
+    [emissionDfc.to_csv(f"./data/{timespan}_{country_code[index]}_emission.csv") for index, emissionDfc in enumerate(emissionDf)]
 
-        # compute total production and emissions
-        totProd = [dfc.apply(np.nansum, axis=1) for dfc in df]
-        totEmit = [emissionDfc.apply(np.nansum, axis=1) for emissionDfc in emissionDf]
+    # compute total production and emissions
+    totProd = [dfc.apply(np.nansum, axis=1) for dfc in df]
+    totEmit = [emissionDfc.apply(np.nansum, axis=1) for emissionDfc in emissionDf]
 
-        # forces axes limit to overlay graphs
-        plt.axes(xlim=(0, 100000), ylim=(0, 650), autoscale_on=False)
+    # forces axes limit to overlay graphs
+    plt.axes(xlim=(0, 100000), ylim=(0, 800), autoscale_on=False)
 
-        # plot
-        [plt.plot(totProd[k], totEmit[k]/totProd[k]/timeStep[k]*1000, '.', markersize=2) for k in range(len(timeStep))]
-        plt.gca().set_ylabel("Emissions (gCO2/kWh)")
-        plt.gca().set_xlabel("Production (MWh)")
-        plt.legend(country_code)
+    # plot
+    [plt.plot(totProd[k], totEmit[k]/totProd[k]/timeStep[k]*1000, '.', markersize=2) for k in range(len(timeStep))]
+    plt.gca().set_ylabel("Emissions (gCO2/kWh)")
+    plt.gca().set_xlabel("Production (MWh)")
+    plt.legend(country_code)
 
-        # save plot to .png
-        filename = f"./plots/{timespan}_{''.join(country_code)}_emission_vs_production.png"
-        plt.savefig(filename, dpi=300, transparent = True)
+    # save plot to .png
+    filename = f"./plots/{timespan}_{''.join(country_code)}_emission_vs_production.png"
+    plt.savefig(filename, dpi=300, transparent = True)
 
-        # clear plot
-        plt.clf()
+    # clear plot
+    plt.clf()
